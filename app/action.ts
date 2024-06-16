5eb437b34561ff829fbd04a99fe25abb01c0765a
Aishwarya Shimpi <aishwaryashimpi2001@gmail.com>
2024-06-16 15:36:12 +0530

    project complete

---
+++ app/action.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export const fetchAnime = async (page: number) => {
+  const respone = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+
+  const data = await respone.json();
+
+  return data;
+};
